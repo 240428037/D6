778aa7a7155b5278e05f4c219422e083d24ba770
240428037 <240428037@qq.com>
2021-04-18 00:13:29 +0800

    提交代码

---
+++ cpp2_20210417/cpp2_20210417/test.cpp
@@ -0,0 +1,72 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//int main()
+//{
+//	printf("hehe\n");
+//	return 0;
+//}
+
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//	cout << "hehe" << endl;
+//}
+
+//#include <stdio.h>
+//namespace syc
+//{
+//	int printf = 0;
+//} 
+//
+//
+//namespace lhy
+//{
+//	int printf = 1;
+//	void f();
+//}
+//void lhy::f()
+//{
+//	::printf("lhy::f()\n");
+//}
+//
+//
+//namespace wh
+//{
+//	int printf = 2;
+//	void f()
+//	{ 
+//		::printf("wh::f()\n");
+//	}
+//}
+//
+//
+//int main()
+//{
+//	printf("%d\n", printf);
+//	printf("%p\n", printf);
+//	printf("%d\n", syc::printf);
+//	printf("%d\n", lhy::printf);
+//	printf("%d\n", wh::printf);
+//	lhy::f();
+//	wh::f();
+//	return 0;
+//}
+
+
+//#include <iostream>
+////using namespace std;
+//int main()
+//{
+//	std::cout << "hehe" << std::endl;
+//}
+
+
+//#include <iostream>
+////using namespace std;
+//using std::cout;
+//using std::endl;
+//int main()
+//{
+//	cout << "hehe" << endl;
+//}
