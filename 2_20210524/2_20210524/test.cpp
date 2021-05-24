609e2e198277aa27a7a1eac1d9719cafcef06da2
240428037@qq.com <xagydx8015>
2021-05-25 01:33:41 +0800

    提交代码

---
+++ 2_20210524/2_20210524/test.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+	ListNode* deleteDuplicates(ListNode* head) {
+		if (!head) {
+			return head;
+		}
+		ListNode* cur = head;
+		while (cur->next) {
+			if (cur->val == cur->next->val) {
+				cur->next = cur->next->next;
+			}
+			else {
+				cur = cur->next;
+			}
+		}
+		return head;
+	}
+};
+
+
+ListNode* cur = head->next, *prev = head;
+while (cur) {
+	if (prev->val == cur->val) {
+		cur = cur->next;
+	}
+	else {
+		cur = cur->next;
+	}
+}
